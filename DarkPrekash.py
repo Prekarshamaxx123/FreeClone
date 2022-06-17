e53ff6e051226daaa8d420bf18c4231c98ebee1e
DarkCyberPrekash <92394796+Prekarshamaxx123@users.noreply.github.com>
2022-06-17 05:53:45 +0530

    Update DarkPrekash.py

--- DarkPrekash.py
+++ DarkPrekash.py
@@ -28,7 +28,7 @@ os.system('xdg-open https://www.facebook.com/PREKASH.I.AM.HACKER.Im.silent')
 def convert(cookie):
 	cookies = {"cookie":cookie}
 	res = requests.Session().get('https://business.facebook.com/business_locations', headers = {
-		'user-agent'	:	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36 Edg/100.0.1185.29',
+		'user-agent'	:	'Mozilla/5.0 (Linux; Android 8.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36',
 		'referer'	:	'https://www.facebook.com/',
 		'host'	:	'business.facebook.com',
 		'origin'	:	'https://business.facebook.com',
